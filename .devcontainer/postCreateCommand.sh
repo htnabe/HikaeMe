ee589e99d1121a09e7e3a612daf6beff1ca1e2a8
htnabe <t_official@t-pot.me>
2025-06-21 07:56:32 +0000

    updated devcontainer settings

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,7 @@
-sudo chown -R vscode /home/linuxbrew/.linuxbrew
-export COREPACK_ENABLE_DOWNLOAD_PROMPT=0 && sudo corepack enable && yarn set version stable && yarn install
+# install homebrew and hugo
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /home/node/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/node/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc hugo sass/sass/sass
+sudo corepack enable && sudo yarn set version stable && yarn install
